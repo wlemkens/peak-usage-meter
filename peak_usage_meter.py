a1aa92f40d3662a1ab460949d91a76a9b6cd3dfd
Wim Lemkens <wim.lemkens@gmail.com>
2023-02-16 23:42:55 +0100

    Added some debug info

--- peak_usage_meter.py
+++ peak_usage_meter.py
@@ -44,24 +44,25 @@ class PeakPowerMeter():
             du = current_consumption - self.first_consumption
             usage = du * 1000 / dt * 3600
             self.client.publish("energy/current_peak_usage", usage, 2, False)
+            logging.debug(
+                f'{datetime.now().strftime("%H:%M:%S")} Peak usage {usage}W')
             if now.minute % 15 < self.first_time.minute % 15:
                 # New time block
                 self.first_time = now
                 self.first_consumption = current_consumption
                 if self.highest_usage < usage:
+                    logging.info(
+                        f'{datetime.now().strftime("%H:%M:%S")} Peak usage increased from = {self.highest_usage}W to {usage}W.')
                     self.highest_usage = usage
                 self.client.publish("energy/peak_usage", self.highest_usage, 2, True)
                 if now.day == 1:
+                    logging.info(
+                        f'{datetime.now().strftime("%H:%M:%S")} Peak usage reset because of new month.')
                     self.highest_usage = 0
-
-            print(f"Usage = {usage}W")
         else:
             self.first_time = now
             self.first_consumption = float(message.payload)
         self.first_time = now
-        print("Received message")
-        print(float(message.payload))
-        print(message.timestamp)
 
 def main(argv):
     meter = PeakPowerMeter()
